be6f4350bf118763c92df46e3fb9a23dd7127777
Jan Cvek <jan.cvek@gmail.com>
2020-11-28 19:58:41 +0100

    bugfix pildek.js

--- pildek.js
+++ pildek.js
@@ -19,6 +19,10 @@ console.log("Listening on port 4000...");
 // ONLY IN PRODUCTION
 // routing to index.html
 app.use(express.static(path.join(__dirname,'build','index.html')));
+
+// routing to static files
+app.use(express.static(path.join(__dirname,'build')));
+
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'build','index.html'));
 });
